4815a3564f3e167ef412fbf67f70c94afcf3c093
Chi Zeng <chi@chizeng.com>
2017-03-04 18:04:13 -0800

    Removed feedback link.

--- src/raw_scripts/service-worker.js
+++ src/raw_scripts/service-worker.js
@@ -21,7 +21,7 @@
 // NOTE(chizeng): UPPERCASE constants are preprocessed (replaced
 // with actual values) during building.
 
-var cacheVersion = 'CACHE_VERSION_24';
+var cacheVersion = 'CACHE_VERSION_25';
 
 this.addEventListener('install', function(event) {
   // Install the service worker.
